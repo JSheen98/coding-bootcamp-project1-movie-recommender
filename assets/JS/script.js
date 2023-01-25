b6d931c4ac39d438630f846d560113ecf3b3f5f8
Jsheen98 <jsheendev@gmail.com>
2023-01-25 16:52:25 -0700

    Fixed position for the map modal button

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -12,3 +12,19 @@ mapButton.addEventListener('click', function () {
 mapModalBg.addEventListener('click', function() {
     mapModal.classList.remove('is-active')
 })
+
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
